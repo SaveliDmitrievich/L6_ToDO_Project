8e0cb8b2b806701ee6c24887307174648ec3a504
ėxpeċtatıöŋ sorry <dmitrievichstud@gmail.com>
2025-11-01 08:25:53 +0300

    обнова рендера в индексе

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import render from './Generator/generator.js'
 import { headerStruct } from './Structs/HeaderStruct.js'
+import { getBreadcrumbsStructure } from './Utils/breadCrumbs.js'
+import { parseHash, getMainStructure } from './Utils/router.js'
+import { footerStruct } from './Structs/FooterStruct.js'
+import { searchStruct } from './Structs/SearchStruct.js'
 
-const Header = headerStruct()
-const parent = document.getElementById('container')
+const root = document.getElementById('container')
 
-render(Header, parent)
+function renderPage() {
+	const path = parseHash()
+	const mainStructure = getMainStructure(path)
+
+	const pageStructure = {
+		tag: 'div',
+		attributes: { class: 'wrapper' },
+		children: [
+			headerStruct(),
+			searchStruct(),
+			getBreadcrumbsStructure(),
+			mainStructure,
+			footerStruct(),
+		],
+	}
+
+	root.innerHTML = ''
+	render(pageStructure, root)
+}
+
+window.addEventListener('hashchange', renderPage)
+window.addEventListener('load', renderPage)
